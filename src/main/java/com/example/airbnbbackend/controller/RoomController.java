458cdd0f4b21847762bb3bfd5b9b229b8ee21c26
Minwoo Lee <rkdxhd97@naver.com>
2024-03-27 14:40:15 +0900

    Chore: Controller 디렉토리 세팅
    - 공통 컨트롤러 클래스 생성
      - CategoryController
      - HostController
      - ReviewController
      - RoomController
      - UserController

---
+++ src/main/java/com/example/airbnbbackend/controller/RoomController.java
@@ -0,0 +1,29 @@
+package com.example.airbnbbackend.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/room")
+@Slf4j
+public class RoomController {
+
+    /**
+     * 숙소 상세 페이지 : 숙소 정보 반환 API
+     * @param id
+     * @return
+     */
+//    @GetMapping("/roomDetail/{id}")
+//    public ResponseEntity<EachRoomResponseDto> getRoomPage(@PathVariable Long id) {
+//    }
+
+    /**
+     * 홈 페이지 : 숙소 정보 리스트 반환 API
+     */
+//    @GetMapping("/")
+//    public List<HomepageRoomResponseDto> getRoomPage() {
+//    }
+
+
+}
---
+++ src/main/java/com/example/airbnbbackend/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.example.airbnbbackend.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/user")
+@Slf4j
+public class UserController {
+}
