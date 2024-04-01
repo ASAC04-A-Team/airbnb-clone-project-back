ddf5f33fbc90336265224d2dd1db0bfc9c4bc774
Minwoo Lee <rkdxhd97@naver.com>
2024-04-01 17:30:09 +0900

    Feat: room reviews 반환 API 구현
    - 방 번호를 통해 각 방의 리뷰를 리스트로 가져오는 API 구현

---
+++ src/main/java/com/example/airbnbbackend/repository/ReviewRepository.java
@@ -0,0 +1,13 @@
+package com.example.airbnbbackend.repository;
+
+import com.example.airbnbbackend.domain.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    List<Review> findReviewByRoomId(Long roomId);
+}
---
+++ src/main/java/com/example/airbnbbackend/service/ReviewService.java
@@ -0,0 +1,30 @@
+package com.example.airbnbbackend.service;
+
+import com.example.airbnbbackend.domain.Review;
+import com.example.airbnbbackend.dto.responseDto.EachRoomReviewResponseDto;
+import com.example.airbnbbackend.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
+    /**
+     * 방 번호에 따른 리뷰리스트 반환
+     * @param roomId
+     * @return 리뷰 리스트
+     */
+    public List<EachRoomReviewResponseDto> findAllRoomReviews(Long roomId) {
+        List<Review> reviews = reviewRepository.findReviewByRoomId(roomId);
+        return reviews.stream().map((eachReview) ->
+                EachRoomReviewResponseDto.of(eachReview))
+                .toList();
+    }
+}
