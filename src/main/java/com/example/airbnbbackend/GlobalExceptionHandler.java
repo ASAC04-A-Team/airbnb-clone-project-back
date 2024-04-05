4de9e014f8e8ec210aafce56ef749a1dc615bb3a
“Winsomejoo” <tmdwn0603@naver.com>
2024-04-05 09:19:54 +0900

    fix: gitignore에 logs파일 ë등록

--- src/main/java/com/example/airbnbbackend/GlobalExceptionHandler.java
+++ src/main/java/com/example/airbnbbackend/GlobalExceptionHandler.java
@@ -17,11 +17,11 @@ import static com.example.airbnbbackend.config.BaseResponseStatus.GlOBAL_EXCEPTI
 @Slf4j
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(value = BaseException.class)
-//    public ResponseEntity<Object> ExceptionHandler(BaseException e) {
-//        //log.error("MethodArgumentNotValidException", e);
-//        return new ResponseEntity<>(new BaseResponse<>(e.getStatus()),HttpStatus.NOT_FOUND);
-//    }
+    @ExceptionHandler(value = BaseException.class)
+    public ResponseEntity<Object> ExceptionHandler(BaseException e) {
+        //log.error("MethodArgumentNotValidException", e);
+        return new ResponseEntity<>(new BaseResponse<>(e.getStatus()),HttpStatus.NOT_FOUND);
+    }
 
     //예외 전역 처리
     /*TODO 로그 파일 생성하기*/
